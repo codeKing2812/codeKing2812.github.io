21d2800557f3f4cd18b950dea3af7876d5ddabc4
codeKing2812 <linus.gbh@gmail.com>
2022-09-20 15:07:11 +0200

    oppdatert programmet totalt, har bla. lagt til en mer avansert grid og kommentert ut bondesjakk, samt lagt til at designet endrer seg ved minde skjerm

---
+++ grid/grid.js
@@ -0,0 +1,13 @@
+// var bondesjakk = document.querySelector('#bondesjakk');
+// var spillBoks = document.querySelector('.spill');
+
+// function loop() {
+//     console.log(bondesjakk.style.width)
+// if (bondesjakk.style.width > spillBoks.style.width) {
+//     bondesjakk.style.width = '100%';
+// } else {
+//     bondesjakk.style.height = '100%';
+// }
+// }
+
+// setInterval(loop, 50)
