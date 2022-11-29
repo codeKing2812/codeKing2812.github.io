7b0b11c7839c4cc5b0c28638af7a3ae32ddcd9f2
codeKing2812 <linus.gbh@gmail.com>
2022-11-29 15:50:50 +0100

    oppdaterte parametre i constructor, og lagt til kontrolller for begge karakterene

--- box/box.js
+++ box/box.js
@@ -3,23 +3,31 @@ const canvas = document.getElementById('mycanvas');
 const ctx = canvas.getContext('2d');
 
 class box {
-    constructor (x, y, xv, yv, width, height, friksjon, maksFart, farge) {
-    this.x = 240;
-    this.y = 240;
-    this.xv = 0;
-    this.yv = 0;
-    this.width = 20;
-    this.height = 20;
-    this.friksjon = 0.05;
-    this.maksFart = 7;
-    this.farge = 'black';
-    }
-}
+    constructor (sheetX, sheetY, sheetWidth, sheetHeight, x, y, width, height, xv, yv, friksjon, bilde) {
+    this.sheetX = sheetX,
+    this.sheetY = sheetY,
+    this.sheetWidth = sheetWidth,
+    this.sheetHeight = sheetHeight,
+    this.x = x,
+    this.y = y,
+    this.width = width,
+    this.height = height,
+    this.xv = xv,
+    this.yv = yv,
+    this.friksjon = friksjon,
+    this.bilde = bilde;
+}}
 
 box.prototype.draw = function() {
-    ctx.fillRect(this.x, this.y, this.width, this.height); //tegn
-    ctx.fillStyle = this.farge;
-
+    ctx.drawImage(this.bilde, this.sheetX, this.sheetY, this.sheetWidth, this.sheetHeight, this.x, this.y, this.width, this.height);
+    console.log(this.sheetX);
+    
+    if ((frame/20) % 1 == 0) {
+        if (this.sheetX < this.bilde.width) {
+            this.sheetX += this.sheetWidth;
+        } else {this.sheetX = 0;}
+    }
+    
     this.x += this.xv;
     this.y += this.yv; //flytt
 
@@ -28,37 +36,96 @@ box.prototype.draw = function() {
     if (this.xv < 0) {this.xv += this.friksjon;}
     if (this.xv > 0) {this.xv -= this.friksjon;}//friksjon
 
-    if (this.yv < -this.maksFart) {this.yv = -this.maksFart;}
-    if (this.yv > this.maksFart) {this.yv = this.maksFart;}
-    if (this.xv < -this.maksFart) {this.xv = -this.maksFart;}
-    if (this.xv > this.maksFart) {this.xv = this.maksFart;}//maksfart
-
     if(this.x > canvas.width) {this.x = 0 - this.width;}
     if(this.x + this.width < 0) {this.x = canvas.width;}
     if(this.y > canvas.height) {this.y = 0 - this.height;}
     if(this.y + this.height < 0) {this.y = canvas.height;} //bli på kartet  
 
-} 
-
-function keyDetector (key) {
-    if (key.keyCode == 87 ) {selv.yv -= 2;} //opp
-    if (key.keyCode == 83 ) {selv.yv += 2;} //ned
-    if (key.keyCode == 65 ) {selv.xv -= 2;} //høyre
-    if (key.keyCode == 68 ) {selv.xv += 2;} //venstre
 }
 
-let selv = new box();
-let slem = new box();
+let Wtast = false;
+let Stast = false;
+let Dtast = false;
+let Atast = false;
 
+let OppTast = false;
+let NedTast = false;
+let VenstreTast = false;
+let HøyreTast = false;
 
+addEventListener("keydown", function(e) {
+    let tast = e.key;
+    console.log(tast);
 
+    if(e.key==="w") {
+        Wtast = true;
+    }
+    if(e.key==="s") {
+        Stast = true;
+    }
+    if(e.key==="a") {
+        Atast = true;
+    }
+    if(e.key==="d") {
+        Dtast = true;
+    }
+
+    if(e.key==="ArrowUp") {
+        OppTast = true;
+    }
+    if(e.key==="ArrowDown") {
+        NedTast = true;
+    }
+    if(e.key==="ArrowLeft") {
+        VenstreTast = true;
+    }
+    if(e.key==="ArrowRight") {
+        HøyreTast = true;
+    }
+
+});
+
+addEventListener("keyup", function() {
+    Wtast = false;
+    Stast = false;
+    Atast = false;
+    Dtast = false;
+
+    OppTast = false;
+    NedTast = false;
+    VenstreTast = false;
+    HøyreTast = false;
+});
+
+const spritesheetGrønn = new Image();
+spritesheetGrønn.src = 'bilder/Snurrehattmann.png';
+
+const spritesheetRød = new Image();
+spritesheetRød.src = 'bilder/Flosshattmann.png';
+
+let grønn = new box(0, 0, 100, 100, 250, 250, 150, 150, 0, 0, 0.2, spritesheetGrønn);
+let rød = new box(0, 0, 100, 100, 350, 350, 150, 150, 0, 0, 0.2, spritesheetRød);
+
+let frame = 0;
 function loop() { 
+    frame++;
+
     ctx.clearRect(0,0,canvas.width,canvas.height);
-    selv.draw();
-    slem.draw();
+    grønn.draw();
+    rød.draw();
+
+    if (Wtast) {grønn.yv -= 1;} //opp
+    if (Stast) {grønn.yv += 1;} //ned
+    if (Atast) {grønn.xv -= 1;} //venstre
+    if (Dtast) {grønn.xv += 1;} //høyre
+
+    if (OppTast) {rød.yv -= 1;} //opp
+    if (NedTast) {rød.yv += 1;} //ned
+    if (VenstreTast) {rød.xv -= 1;} //venstre
+    if (HøyreTast) {rød.xv += 1;} //høyre
 
     requestAnimationFrame(loop)
 }
+loop();
 
 // setInterval(loop,50);
-addEventListener('keydown',keyDetector);
