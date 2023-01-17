3f1b21976a1e3acd64c299f66b577a451d14117d
codeKing2812 <linus.gbh@gmail.com>
2023-01-17 14:53:40 +0100

    Arbeidet med påskerenn-oppgave, og laget en interaktiv tabell for deltakere

--- json/json.js
+++ json/json.js
@@ -1,25 +1,64 @@
+const oversikt = document.querySelector('#oversikt');
+const tabell = document.querySelector('#tabell');
+document.querySelector('#skjema_nyPerson').addEventListener('submit', nyPerson);
+document.querySelector('#slettKnapp').addEventListener('click', slettForrige);
+tabell.addEventListener('click', rediger);
+document.querySelector('#plussKnapp').addEventListener('click', nyRad);
+
+
 let personer = [
-    {navn: 'per', født: 1984},
-    {navn: 'ole', født: 2006},
-    {navn: 'espen', født: 2010},
-    {navn: 'mikkel', født: 1964},
-    {navn: 'roger', født: 2003},
-    {navn: 'lise', født: 1999},
+    {navn: 'Hogne', alder: 23, akt1: 0, akt2: 0, akt3: 0, tot: 0},
+    {navn: 'Linus', alder: 17, akt1: 0, akt2: 0, akt3: 0, tot: 0},
+    {navn: 'JoBjo', alder: 42, akt1: 0, akt2: 0, akt3: 0, tot: 0},
+    {navn: 'Britt-Helen', alder: 29, akt1: 0, akt2: 0, akt3: 0, tot: 0},
 ];
 
-for (let person of personer) {
-    // person.alder = new Date().getFullYear() - person.født;
-    // if (person.alder < 18)
-    // console.log(person.navn + ' er under 18 år');
+oppdaterDeltakerListe();
 
-    person.bool = true;
+function nyPerson(event) {
+    event.preventDefault(); // single page app som betyr at vi ikke forlater nettsiden
+    let innNavn = document.querySelector('#navn').value;
+    let innAlder = document.querySelector('#alder').value;
+    personer.push({navn: innNavn, alder: innAlder, akt1: 0, akt2: 0, akt3: 0, tot: 0});
+
+    oppdaterDeltakerListe();
 };
-personer[2].bool = false;
 
-console.log(personer.every(sjekk))
+function oppdaterDeltakerListe() {
+    tabell.innerHTML = '<tr> <th>Deltaker</th> <th>Alder</th> <th>Akt. 1</th> <th>Akt. 2</th> <th>Akt. 3</th> <th>Total</th> </tr>';
+    
+    for (let person of personer) {
+        let rad = document.createElement('tr');
+        tabell.appendChild(rad);  
+        rad.innerHTML += '<td>' + person.navn + '</td>' + '<td>' + person.alder + '</td>' + '<td>' + person.akt1 + '</td>' + '<td>' + person.akt2 + '</td>' + '<td>' + person.akt3 + '</td>' + '<td>' + person.tot + '</td>';
+    };
+    console.log(personer)
+};
 
-function sjekk(denne) {
-    if (denne.bool) {
-        return true;
+function slettForrige() {
+    personer.pop();
+    oppdaterDeltakerListe();
+    console.log(personer);
+};
+
+let elements;
+let redigerFelt;
+function rediger() {
+    elements = document.querySelectorAll(':hover');
+    elements[elements.length - 1].innerHTML = '<input type="text" autocomplete="off" id="rediger">';
+    redigerFelt = document.querySelector('#rediger');
+    redigerFelt.focus();
+    redigerFelt.addEventListener('keydown', sluttRediger);
+}
+
+function sluttRediger(e) {
+    if (e.key == 'Enter') {
+        console.log(redigerFelt.value)
+        elements[elements.length - 1].innerHTML = '<td>' + redigerFelt.value + '</td>';
     }
-};
+}
+
+function nyRad() {
+    personer.push({navn: '', alder: '', akt1: 0, akt2: 0, akt3: 0, tot: 0});
+    oppdaterDeltakerListe();
+}
