5d8a85acf87740402adb1bfb6bad04fe2f99ffca
codeKing2812 <linus.gbh@gmail.com>
2023-02-08 22:22:02 +0100

    jobbet på randomuser-siden

--- randomuser/randomuser.js
+++ randomuser/randomuser.js
@@ -1,18 +1,22 @@
 let antallBrukere = 0;
+let loading = false;
 let inputAntall = document.createElement('input');
 document.body.appendChild(inputAntall);
 inputAntall.type = 'number';
 
 inputAntall.addEventListener('keyup', function () {
-    antallBrukere = inputAntall.value;
-    // console.log('enter')
-    oppdaterFolk()
+    if (!loading) {
+        loading = true;
+        antallBrukere = inputAntall.value;
+        // console.log('enter')
+        oppdaterFolk()
+    }
 })
 
 function oppdaterFolk() {
-    ut.innerText = '';
+    main.innerText = 'Loading...';
 
-    fetch('https://randomuser.me/api/?results=' + antallBrukere)
+    fetch('https://randomuser.me/api/?nat=no&results=' + antallBrukere)
         .then(response => response.json())
         // .then(response => console.log(response))
         .then(response => behandleSvar(response))
@@ -20,55 +24,78 @@ function oppdaterFolk() {
 
 }
 
-function behandleSvar(svar) {
+function behandleSvar(svar) {  
+    main.innerText = '';
+    
     for (bruker of svar.results) {
     console.log(svar);
     console.log('Navn: ' + bruker.name.first);
 
+    let litenDiv = document.createElement('div');
+    main.appendChild(litenDiv);
+    litenDiv.setAttribute('class', 'litenDiv');    
+
     let navn = document.createElement('p');
-    ut.appendChild(navn);
-    navn.innerText += bruker.name.title + ' ' + bruker.name.first + ' ' + bruker.name.last;
+    litenDiv.appendChild(navn);
+    navn.innerText += bruker.name.first + ' ' + bruker.name.last;
     
     let alder = document.createElement('p');
-    ut.appendChild(alder);
-    navn.innerText += ', som er ' + bruker.dob.age + ' år gammel';
+    litenDiv.appendChild(alder);
+    alder.innerText += bruker.dob.age + ' år gammel';
+
+    let sted = document.createElement('p');
+    litenDiv.appendChild(sted);
+    sted.innerText += 'Fra ' + bruker.location.city;
 
     let bilde = document.createElement('img');
-    ut.appendChild(bilde);
+    litenDiv.appendChild(bilde);
     bilde.setAttribute('src', bruker.picture.large);
+    bilde.setAttribute('class', 'brukerBilde');
 
-    bilde.addEventListener('mouseover', function () {
-        bilde.style.scale = '1.1';
+    
+
+    litenDiv.addEventListener('mouseover', function () {
+        litenDiv.style.scale = '1.1';
+        bilde.style.scale = '1.0';
     })
-    bilde.addEventListener('mouseleave', function () {
+    litenDiv.addEventListener('mouseleave', function () {
+        litenDiv.style.scale = '1.0';
         bilde.style.scale = '0.8';
     })
+
+    loading = false;
 }}
 
 let overskrift = document.createElement('h1');
 document.body.appendChild(overskrift);
-overskrift.innerText = 'Ansatte ved IT avdelingen ved ASVG';
+overskrift.innerText = 'Medlemmer i SIAN';
 overskrift.setAttribute('class', 'bakgrunn');
 
-let ut = document.createElement('div');
-document.body.appendChild(ut);
+let logo = document.createElement('img');
+overskrift.appendChild(logo);
+logo.setAttribute('src', 'bilder/sianLogo.jpg');
+logo.setAttribute('id', 'logo');
 
-let høyde = 50;
-overskrift.style.height =  høyde + 'px';
-let bredde = 600;
-overskrift.style.width =  bredde + 'px';
+let main = document.createElement('div');
+document.body.appendChild(main);
+main.setAttribute('id', 'main');
 
-document.addEventListener('keydown', function (e) {
-    let tast = e.code;
-    if (tast === 'KeyW') {
-        høyde -= 2;
-    } else if (tast === 'KeyS') {
-        høyde += 2;
-    } else if (tast === 'KeyD') {
-        bredde += 2;
-    } else if (tast === 'KeyA') {
-        bredde -= 2;
-    }
-    overskrift.style.height =  høyde + 'px';
-    overskrift.style.width =  bredde + 'px';
-})
+// let høyde = 50;
+// overskrift.style.height =  høyde + 'px';
+// let bredde = 600;
+// overskrift.style.width =  bredde + 'px';
+
+// document.addEventListener('keydown', function (e) {
+//     let tast = e.code;
+//     if (tast === 'KeyW') {
+//         høyde -= 2;
+//     } else if (tast === 'KeyS') {
+//         høyde += 2;
+//     } else if (tast === 'KeyD') {
+//         bredde += 2;
+//     } else if (tast === 'KeyA') {
+//         bredde -= 2;
+//     }
+//     overskrift.style.height =  høyde + 'px';
+//     overskrift.style.width =  bredde + 'px';
+// })
