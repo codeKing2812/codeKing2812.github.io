49492bfee1623c91098ec71b72ebed6573feac5a
codeKing2812 <linus.gbh@gmail.com>
2023-09-05 17:01:29 +0200

    begynt å lage pong
    har endret litt mer på funksjonene og begynt å lage faktisk pong

--- IT2/tull/visuellTerminal.py
+++ IT2/tull/visuellTerminal.py
@@ -13,70 +13,70 @@ class design:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
-fargetpx = design.HEADER + 'o ' + design.END
 
 
-def piksel(x, y, farge):
+def tegn(x, y, farge):
     if farge == 'l':
         rader[y][x] =  (design.HEADER)
+    elif farge == 'r':
+        rader[y][x] =  (design.FAIL)
     elif farge == 'b':
-        rader[y][x] =  (design.BLUE)
-    elif farge == 'g':
-        rader[y][x] =  (design.GREEN)
+        rader[y][x] =  (design.CYAN)
         
-    rader[y][x] += ('o ' + design.END)
+    rader[y][x] += ('O ' + design.END)
 
 
 
-xpos = 0
-ypos = 0
+y1 = 0
+y2 = 0
+ball = {'x': 12, 'y': 7, 'xv': 0, 'yv': 0}
 
 tid = time.time()
-for i in range(20):
-    while time.time() < tid + 30:
-        if time.time() > tid + 0.1: # kjør hvert tiendedels sekund
+startTid = tid
+while time.time() < startTid + 60:
+    if time.time() > tid + 0.1: # kjør hvert tiendedels sekund
+
+        os.system('cls')
+        tid += 0.1 
+        # refresh skjermen
+        
+        rader = []
+        for i in range(15):
+            rad = ['O ']*25
+            rader.append(rad)
+        # nullstill alle rader
+
+        
+        if keyboard.is_pressed('w'):
+            y1 -= 1
+        elif keyboard.is_pressed('s'):
+            y1 += 1
+
+        if keyboard.is_pressed('Up'):
+            y2 -= 1
+        elif keyboard.is_pressed('Down'):
+            y2 += 1
+        # bevegelse/key lytter
+
+
+        tegn(ball['x'], ball['y'], 'r')
+
+        tegn(1, y1, 'b')
+        tegn(1, y1+1, 'b')
+        tegn(1, y1+2, 'b')
+
+        tegn(-2, y2, 'l')
+        tegn(-2, y2+1, 'l')
+        tegn(-2, y2+2, 'l')
+
+
+        for rad in rader:
+            pikselString = ''
+            for piksel in rad:
+                pikselString += piksel #lager en string av raden
+            print(pikselString) # skriver ut stringen
             
-            rader = []
-            for i in range(10):
-                rad = ['o ']*25
-                rader.append(rad)
-            # Reset alle rader
-
-
-            os.system('cls')
-            tid += 0.1 
-            #refresh skjermen
-
-            
-            if keyboard.is_pressed('d'):
-                xpos += 1
-            elif keyboard.is_pressed('a'):
-                xpos -= 1
-            if keyboard.is_pressed('w'):
-                ypos -= 1
-            elif keyboard.is_pressed('s'):
-                ypos += 1
-            # bevegelse/key lytter
-
-
-            # piksel(xpos+3, ypos, 'l')
-            # piksel(xpos+3, ypos+2, 'g')
-            #
-            #FUNKER IKKE (ENDA)
-
-            rader[ypos][xpos] =  (fargetpx)
-            rader[ypos+1][xpos+1] = (fargetpx)
-
-            
-            for rad in rader:
-                pikselString = ''
-                for piksel in rad:
-                    pikselString += piksel #lager en string av raden
-                print(pikselString) # skriver ut stringen
-                
-
+print(design.FAIL + 'G A M E  O V E R' + design.END)
 
 
             
-
-
