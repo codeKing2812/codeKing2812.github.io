f2176170effd925cd095e1b8533690915a26279b
codeKing2812 <linus.gbh@gmail.com>
2023-09-19 15:50:26 +0200

    ferdigstilt geoquiz (?)
    har kanskje laget ferdig geoquiz. har jobbet mest med å få tilgang til flere nivåer av svar. nå er det stygt men funker ganske bra

--- IT2/spørrreprogram/geoQuiz.py
+++ IT2/spørrreprogram/geoQuiz.py
@@ -1,22 +1,47 @@
 import json
+import random
 
 fil = open('landData.json', 'r')
 
 landListe = json.loads(fil.read())[1]
 
-print(type(landListe))
+# print(type(landListe))
 
+
+# fjerner alle land uten hovedstad
+nyListe = [] 
 for land in landListe:
-    if land['capitalCity'] == "" :
-        landListe.pop(land)
-    print(land['capitalCity'])
+    if land['capitalCity'] != "" :
+        nyListe.append(land)
+landListe = nyListe
 
 
-temp_dict = {}
-tKey = ""
+# print(landListe[5]['incomeLevel']['value'])
 
-for k, v in landListe.items():
-    if k != tKey:
-        temp_dict[k] = v
+# Quiz
 
-dict = temp_dict
+def spørsmål(kategori, underkat=0):
+
+    land = random.choice(landListe)
+    fasit = ''
+
+    svar = input(f'Hva er {kategori} i {land["name"]}?')
+
+    if str(type(land[kategori])).find('dict') == -1:
+        #hvis kategorien ikke er et dictionary
+        fasit = land[kategori]
+    else: 
+        #hvis kategorien er et dictionary gå et hakk dypere
+        fasit = land[kategori][underkat]
+
+     
+
+    if svar.casefold() == fasit.casefold() :
+        print('Hurra! Det er rett!')
+    else:
+        print(f'Du suger. Det er selvfølgelig {fasit}')
+
+spørsmål("capitalCity")
+
+spørsmål("incomeLevel", "value")
+
