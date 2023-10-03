2472194cca99202b6d57baab87cd1745bbb6f6ac
codeKing2812 <linus.gbh@gmail.com>
2023-10-03 15:36:12 +0200

    laget vegvesen api-program med mål om å finne mammas gamle russebil

---
+++ IT2/api/vegvesen.py
@@ -0,0 +1,26 @@
+import requests as req
+import json
+
+nøkkel = '399c2091-bd5d-49da-a397-01796c492efa'
+
+api_url = 'https://www.vegvesen.no/ws/no/vegvesen/kjoretoy/felles/datautlevering/enkeltoppslag/kjoretoydata?kjennemerke=SP26051'
+
+headers = {"SVV-Authorization" : "Apikey 399c2091-bd5d-49da-a397-01796c492efa"}
+
+response = req.get(api_url, headers = {"SVV-Authorization" : "Apikey 399c2091-bd5d-49da-a397-01796c492efa"})
+
+# response.json()
+
+data = response.content
+
+fil = open('bilData.json', 'w')
+fil.write(str(data)[2:-1])
+fil.close()
+
+
+data = json.loads(data)
+
+merke = data['kjoretoydataListe'][0]['godkjenning']['tekniskGodkjenning']['tekniskeData']['generelt']['merke'][0]['merke']
+modell = data['kjoretoydataListe'][0]['godkjenning']['tekniskGodkjenning']['tekniskeData']['generelt']['handelsbetegnelse'][0]
+
+print('Bilen er en:', merke, modell)
