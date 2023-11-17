aafacf646844e1b70cdc4672bc770af23280a473
codeKing2812 <linus.gbh@gmail.com>
2023-11-17 10:04:53 +0100

    laget en versjon der alt funker
    har fikset hold på kartet og bots som gikk i hverandre så nå burde alt funke

--- IT2/spill/canvasAgario.py
+++ IT2/spill/canvasAgario.py
@@ -5,7 +5,7 @@ import time
 import keyboard
 
 
-# NIO: k3mscqfx
+# NIO: k3mscqfx 
 
 app = Tk()
 app.geometry("500x500")
@@ -14,26 +14,29 @@ c.pack()
 
 
 class Boks:
-    def __init__(self, farge, x, y, masse, state=NORMAL):
+    def __init__(self, farge, x, y, masse, state=NORMAL, respawn='på'):
         self.farge = farge
         self.x = x
         self.y = y
         self.masse = masse
         self.state = state
-        self.respawn = 'på'
+        self.respawn = respawn
         self.startFrame = 0
+
 
     @property
     def bredde(self):
         return (sqrt(self.masse))/2
     
+
     def tegn(self):
         self.id = c.create_rectangle(self.x-self.bredde, self.y-self.bredde, self.x+self.bredde, self.y+self.bredde, fill=self.farge, state=self.state)
     
+
     def endreState(self, state):
-        # print(state)
         self.state = state
         c.itemconfigure(self.id, state=state)
+
 
     def oppdater(self):
         if self.respawn == 'aktiv':
@@ -42,7 +45,8 @@ class Boks:
                 self.endreState(NORMAL)
                 self.respawn = 'på'
     
-    def slett(self):
+
+    def dø(self):
         self.endreState(HIDDEN)
 
         if self.respawn == 'på':
@@ -74,40 +78,54 @@ class Boks:
 
 
 class Spiller(Boks):
-    def __init__(self, farge, x, y, masse, xv=0, yv=0): 
-        super().__init__(farge, x, y, masse)
+    def __init__(self, farge, x, y, masse, xv=0, yv=0, respawn='av'): 
+        super().__init__(farge, x, y, masse, respawn=respawn)
         self.xv = xv
         self.yv = yv
 
-        self.respawn = 'av'
-
         self.tegn()
 
-    def oppdater(self):
-        self.flytt()
 
-        if self.respawn == 'aktiv':
-            global frameNr
-            if frameNr > self.startFrame + random.randint(60, 600):
-                self.endreState(NORMAL)
+    def oppdater(self):
+        super().oppdater()
+
+        if self.state == NORMAL:
+            self.flytt()
+
+
+    def holdPåKartet(self):   
+        if self.x <= 0:
+            self.xv = 0.1
+        elif self.x >= 500:
+            self.xv = -0.1
+
+        if self.y <= 0:
+            self.yv = 0.1
+        elif self.y >= 500:
+            self.yv = -0.1
+    
 
     def flytt(self):
+
+        self.holdPåKartet()
 
         self.x = self.x + self.xv
         self.y = self.y + self.yv
         
         c.coords(self.id, self.x-self.bredde, self.y-self.bredde, self.x+self.bredde, self.y+self.bredde)
     
+
     def spis(self, annen):
         if annen.state == self.state == NORMAL and self.masse > annen.masse:
             self.masse += annen.masse
-            annen.slett()
+            annen.dø()
 
 
 
 class Bot(Spiller):
-    def __init__(self, farge, x, y, masse, xv=0, yv=0):
-        super().__init__(farge, x, y, masse, xv, yv)
+    def __init__(self, farge, x, y, masse, xv=0, yv=0, respawn='av'):
+        super().__init__(farge, x, y, masse, xv, yv, respawn)
+
 
     def jakt(self, alleMål, hovedMål):
         minsteAvstand = 100000
@@ -118,7 +136,8 @@ class Bot(Spiller):
                 minsteAvstand = avstand
                 mål = annen 
 
-        if annen.masse > self.masse : # hvis den ikke er spiselig
+        if mål.masse >= self.masse : # hvis den ikke er spiselig
+            minsteAvstand = 100000
             for annen in alleMål: #finn det nærmeste som er spiselig 
                 avstand = self.avstand(annen)
                 if avstand < minsteAvstand and annen.masse < self.masse and annen.state == NORMAL:
@@ -126,28 +145,27 @@ class Bot(Spiller):
                     mål = annen
 
         if self.x < mål.x:
-            self.xv = 0.15
+            self.xv = 0.3
         else :
-            self.xv = -0.15
+            self.xv = -0.3
             
         if self.y < mål.y:
-            self.yv = 0.15
+            self.yv = 0.3
         else :
-            self.yv = -0.15
+            self.yv = -0.3
 
 
 
-bot1 = Bot('red', 60, 100, 450)
-bot2 = Bot('red', 470, 130, 450)
-bot3 = Bot('red', 390, 480, 450)
-bot4 = Bot('red', 200, 360, 450)
+bot1 = Bot('red', random.randint(0, 500), 100, 500)
+bot2 = Bot('red', random.randint(0, 500), 200, 500)
+bot3 = Bot('red', random.randint(0, 500), 300, 500)
+bot4 = Bot('red', random.randint(0, 500), 400, 500)
 
 botListe = [bot1, bot2, bot3, bot4]
 
-blå = Spiller('blue', 50, 200, 400)
+blå = Spiller('blue', 50, 200, 500)
 
 spillerListe = [blå] + botListe
-
 
 matListe = []
 for y in range(5):
@@ -158,7 +176,7 @@ for y in range(5):
 
 alleBokser = spillerListe + matListe
 
-frameNr = 0
+
 def gameloop() :
     global frameNr
 
@@ -181,32 +199,24 @@ def gameloop() :
         boks.oppdater()
 
 
+
     if keyboard.is_pressed('Right'):
-        blå.xv = 0.2
-        blå.flytt()
+        blå.xv = 0.5
     elif keyboard.is_pressed('Left'):
-        blå.xv = -0.2
-        blå.flytt()
+        blå.xv = -0.5
     else: blå.xv = 0
 
     if keyboard.is_pressed('Down'):
-        blå.yv = 0.2
-        blå.flytt()
+        blå.yv = 0.5
     elif keyboard.is_pressed('Up'):
-        blå.yv = -0.2
-        blå.flytt()
+        blå.yv = -0.5
     else: blå.yv = 0
-
-    
-
-    app.update_idletasks()
-    app.update()
 
     frameNr+=1
 
 
-
-spillLengde = 45
+frameNr = 0
+spillLengde = 30
 framerate = 1/60
 tid = startTid = time.time()
 
@@ -214,6 +224,9 @@ while tid < startTid + spillLengde: # til tiden er ute
     if time.time() > tid: # kjør frame
         gameloop()
         tid += framerate 
+
+        app.update_idletasks()
+        app.update()
          
 
 print('fps:', frameNr/spillLengde)
