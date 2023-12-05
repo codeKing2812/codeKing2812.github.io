61c33e7d3d8915f9a79e5f903a4c14849658e39d
EirikFugl <143164368+EirikFugl@users.noreply.github.com>
2023-12-05 14:14:43 +0100

    Update søtEllerSur.py
    du burde endre dette

--- IT2/tull/søtEllerSur.py
+++ IT2/tull/søtEllerSur.py
@@ -4,12 +4,12 @@ alder = int(input('Alder: '))
 bokstavVerdi = ord(navn[0])
 
 if bokstavVerdi < 80 :
-    smak = 'sur'
+    smak = 'sur, som eirik'
 elif bokstavVerdi < 90 :
-    smak = 'søt'
+    smak = 'søt, som linus'
 elif bokstavVerdi < 110 :
-    smak = 'sur'
+    smak = 'sur, som linus'
 else :
-    smak = 'søt'
+    smak = 'søt, som eirik'
 
 print(f'Du heter {navn} og er en {alder} år gammel {smak} frukt')
